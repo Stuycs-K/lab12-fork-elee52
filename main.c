2a051205f8b86b804230d4fd241065b2de8f8d1d
Ethan Lee <elee52@stuy.edu>
2024-11-26 14:48:46 -0500

    printed pid values and made random num for each fork using pid

--- main.c
+++ main.c
@@ -11,16 +11,27 @@
 //use pid for random
 //sleep that long
 //child dies, parent dies, other child dies
-
-int main(){
+int myfork(){
   pid_t p;
   p = fork();
   if(p<0){
     perror("fork fail");//output to stderr instead of stdout
     exit(1);
   } else if ( p == 0){
-      printf("Hello from Child!\n");
+      printf("Hello from Child!: my pid_t is %d\n", p);
+      srand(p);
+      int r = random()%5;
+      printf("ranodm num:%d\n", r);
+      sleep(r);
   }else{
-      printf("Hello from Parent!\n");
+      printf("Hello from Parent!: my pid_t is %d\n", p);
+      srand(p);
+      int r = random()%5;
+      printf("random num:%d\n", r);
+      sleep(r);
   }
+}
+int main(){
+  myfork();
+  //myfork();
 }
